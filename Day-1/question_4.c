81e80f906a01d056885ad6ab420019bbaab282e1
KhalidSultan <khalidsultan@localhost.localdomain>
2019-11-19 16:08:11 +0300

    Finalizing

--- Day-1/question_4.c
+++ Day-1/question_4.c
@@ -20,82 +20,104 @@ void main(){
     //  = malloc(1000000*sizeof(char));    
     int bigger_size = 0;
 
-    if(first_number_length<=second_number_length){
+    int bigger_one = 0;
+    if(first_number_length<second_number_length){
         bigger_size = second_number_length;
+        bigger_one = 2;
+    }
+    else if(first_number_length>second_number_length){
+        bigger_size = first_number_length;
+        bigger_one = 1;
     }
     else{
-        bigger_size = first_number_length;
+        int temp = 0;
+        while(temp<first_number_length){
+            if(first_number[temp]>second_number[temp]){
+                bigger_size = first_number;
+                bigger_one = 1;
+                break;
+            } 
+            else if(first_number[temp]<second_number[temp]){
+                bigger_size = second_number;
+                bigger_one = 2;
+                break;
+            }            
+            temp+=1;
+        }
     }
 
-    int temp_counter = 0;
     int shifter = 0;
+    
     int bool_check = 0;
+    int positive_or_negative = 1;
+
     if(first_number[0]=='-'){
         bool_check = 1;
     }
     if(second_number[0]=='-'){
         if(bool_check==1) {
             bool_check = 0;
+            positive_or_negative = -1;
         }
         else{
             bool_check = 2;
         }        
     }
+
+    if(bool_check==1 && bigger_one==1){
+        positive_or_negative = -1;
+    }
+    else if(bool_check==2 && bigger_one==2){
+        positive_or_negative = -1;
+    }
+
+    int temp_counter = 0;
     while (temp_counter<bigger_size)
     {
-        char first_number_digit;
-        char second_number_digit;
+        int first_number_digit = ( (char) first_number[first_number_length-1] )- '0';
+        int second_number_digit = ( (char) second_number[second_number_length-1] )- '0';
 
-        if(bool_check==0 || bool_check==1 && first_number_length>0 || bool_check==2 && second_number_length>0){
-            first_number_digit = (char) first_number[first_number_length-1];
-            second_number_digit = (char) second_number[second_number_length-1];
-        } 
-        else if(bool_check==1 && first_number_length==0){
-            first_number_digit = '0';
-            second_number_digit = (char) second_number[second_number_length-1];
-        }         
-        else if(bool_check==2 && second_number_length==0){
-            first_number_digit = (char) first_number[first_number_length-1];
-            second_number_digit = '0';
-        }         
+        int sum = 0;
 
-        if(second_number_digit<48 || second_number_digit>57) second_number_digit = 48;
-        if(first_number_digit<48 || first_number_digit>57) first_number_digit = 48;
-        printf("%c with %c \n", first_number_digit, second_number_digit);
+        if(bool_check==0){
+            if(first_number_digit<0 || first_number_digit>9) first_number_digit = 0;
+            if(second_number_digit<0 || second_number_digit>9) second_number_digit = 0;
+            sum = first_number_digit + second_number_digit + shifter;
+        }
+
+        else if(bool_check==1){
+            if(bigger_one = 1){
+                sum = first_number_digit-second_number_digit;
+            }
+            else{
+                int checker = first_number_digit;
+                int temp = first_number_length-1;
+                while(checker<second_number_digit){
+                    checker = ( (char) first_number[temp] ) - '0';
+                    if(checker<0) first_number[temp] = '9';
+                    else first_number[temp] = 
+                }
+            }
+            else if(first_number_digit>second_number_digit){
+                sum = first_number_digit - second_number_digit;
+            }
+            else{
+                sum = 0;
+            }
+        }
+        else {
+        }
+        printf("%c with %c \n", first_number_digit+48, second_number_digit+48);
 
         first_number_length -= 1;
         second_number_length -= 1;
 
-        int sum = 0;
-        if(bool_check==0){
-            sum = (((int)first_number_digit)-48)+(((int)second_number_digit)-48)+ shifter;
-            if(sum>=10) {
-                sum = sum-10;
-                shifter = 1; 
-            }
-            else{            
-                shifter = 0;
-            }
+        if(sum>=10) {
+            sum = sum-10;
+            shifter = 1; 
         }
-        else if(bool_check==1){
-            sum = ((-1 * (int)first_number_digit)-48)+(((int)second_number_digit)-48)+ shifter;
-            if(sum>=10) {
-                sum = sum-10;
-                shifter = 1; 
-            }
-            else{            
-                shifter = 0;
-            }
-        }
-        else{
-            sum = (((int)first_number_digit)-48)+((-1*(int)second_number_digit)-48)+ shifter;
-            if(sum>=10) {
-                sum = sum-10;
-                shifter = 1; 
-            }
-            else{            
-                shifter = 0;
-            }
+        else{            
+            shifter = 0;
         }
         result[temp_counter] = sum+48;
         temp_counter+=1;
