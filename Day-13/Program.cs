719f1546595a8e1cf131202d2e0baaa77807ff72
Khalid Sultan <Kalid.sultan.95@gmail.com>
2019-12-10 19:50:55 +0300

    Added Day-3 Q1

---
+++ Day-13/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> lines = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                lines.Add(Console.ReadLine());
+            }
+            foreach (string s in lines)
+            {
+                if (check(s) == false)
+                {
+                    Console.WriteLine("-1");
+                }
+                else
+                {
+                    Console.WriteLine(replace(s));
+                }
+            }
+        }
+        static bool check(string s)
+        {
+            char temp = s[0];
+            if (s.Length == 1) return true;
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (temp == s[i] && s[i] != '?') return false;
+                temp = s[i];
+            }
+            return true;
+        }
+        static string replace(string s)
+        {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '?')
+                {
+                    if (i < s.Length - 1 && s[i + 1] != '?')
+                    {
+                        if (builder.Length > 0)
+                        {
+                            if (builder[builder.Length - 1] == 'a')
+                            {
+                                if (s[i + 1] == 'a') builder.Append('b');
+                                else if (s[i + 1] == 'b') builder.Append('c');
+                                else if (s[i + 1] == 'c') builder.Append('b');
+                            }
+                            else if (builder[builder.Length - 1] == 'b')
+                            {
+                                if (s[i + 1] == 'a') builder.Append('c');
+                                else if (s[i + 1] == 'b') builder.Append('c');
+                                else if (s[i + 1] == 'c') builder.Append('a');
+                            }
+                            else if (builder[builder.Length - 1] == 'c')
+                            {
+                                if (s[i + 1] == 'a') builder.Append('b');
+                                else if (s[i + 1] == 'b') builder.Append('a');
+                                else if (s[i + 1] == 'c') builder.Append('b');
+                            }
+                        }
+                        else
+                        {
+                            if (s[i + 1] == 'a') builder.Append('b');
+                            else if (s[i + 1] == 'b') builder.Append('a');
+                            else if (s[i + 1] == 'c') builder.Append('b');
+                        }
+                    }
+                    else
+                    {
+                        if (builder.Length > 0)
+                        {
+                            if (builder[builder.Length - 1] == 'a') builder.Append('b');
+                            else if (builder[builder.Length - 1] == 'b') builder.Append('a');
+                            else if (builder[builder.Length - 1] == 'c') builder.Append('a');
+                        }
+                        else
+                        {
+                            builder.Append('a');
+                        }
+                    }
+                }
+                else
+                {
+                    builder.Append(s[i]);
+                }
+            }
+            return builder.ToString();
+        }
+    }
+}
