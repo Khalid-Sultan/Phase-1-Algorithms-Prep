6ea622a68b514854ff6fff35000a55a4a36d3d08
Khalid Sultan <Kalid.sultan.95@gmail.com>
2019-12-21 11:05:40 +0300

    Added Binary Search

---
+++ Day-19/Binary_Search.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_19
+{
+    class Binary_Search
+    {
+        public int Search(int[] nums, int target)
+        {
+            int lowest = 0;
+            int max = nums.Length - 1;
+            while (lowest <= max)
+            {
+                if (nums[lowest + ((max - lowest) / 2)] == target)
+                {
+                    return lowest + ((max - lowest) / 2);
+                }
+                else if (nums[lowest + ((max - lowest) / 2)] < target)
+                {
+                    lowest = 1 + lowest + ((max - lowest) / 2);
+                }
+                else
+                {
+                    max = -1 + lowest + ((max - lowest) / 2);
+                }
+            }
+            return -1;
+        }
+    }
+}
