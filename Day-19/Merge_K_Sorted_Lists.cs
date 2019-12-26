70b3a1a06c4ebc169e8414cf4aeb4850d8af5d51
Khalid Sultan <Kalid.sultan.95@gmail.com>
2019-12-26 13:24:18 +0300

    Added Merge K Sorted Lists

---
+++ Day-19/Merge_K_Sorted_Lists.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_19
+{
+    class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    class Merge_K_Sorted_Lists
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0) return null;
+            List<int> list = new List<int>();
+            foreach (ListNode l in lists)
+            {
+                ListNode temp = l;
+                while (temp != null)
+                {
+                    list.Add(temp.val);
+                    temp = temp.next;
+                }
+            }
+            if (list.Count == 0) return null;
+            list.Sort();
+            ListNode dummyHead = new ListNode(list[0]);
+            ListNode head = dummyHead;
+            list.RemoveAt(0);
+            while (list.Count > 0)
+            {
+                dummyHead.next = new ListNode(list[0]);
+                list.RemoveAt(0);
+                dummyHead = dummyHead.next;
+            }
+            return head;
+        }
+
+    }
+}
