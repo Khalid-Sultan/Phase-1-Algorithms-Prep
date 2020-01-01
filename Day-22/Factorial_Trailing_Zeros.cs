2bef6e7b25b6a0d75c63f4b87383cd6f4d7d6569
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-01 14:25:43 +0300

    Added Day-22

---
+++ Day-22/Factorial_Trailing_Zeros.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_22
+{
+    class Factorial_Trailing_Zeros
+    {
+        public int TrailingZeroes(int n)
+        {
+            if (n <= 0) return 0;
+            int count = 0;
+            while (n > 0)
+            {
+                count += n / 5;
+                n /= 5;
+            }
+
+            return count;
+        }
+    }
+}
