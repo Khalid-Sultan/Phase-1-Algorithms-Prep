180033113b418633925db9a88d47c8db5d7a55b1
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-02 12:40:44 +0300

    Added Power

--- Day-22/PowXN.cs
+++ Day-22/PowXN.cs
@@ -8,28 +8,8 @@ namespace Day_22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"{MyPow(0, 0)} {MyPow(0, 0) == Math.Pow(0, 0)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(0, 1)} {MyPow(0, 1) == Math.Pow(0, 1)}");
-            Console.WriteLine($"{MyPow(0, -1)} {MyPow(0, -1) == Math.Pow(0, -1)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 4)} {MyPow(2, 4) == Math.Pow(2, 4)}");
-            Console.WriteLine($"{MyPow(2, -4)} {MyPow(2, -4) == Math.Pow(2, -4)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 5)} {MyPow(2, 5) == Math.Pow(2, 5)}");
-            Console.WriteLine($"{MyPow(2, -5)} {MyPow(2, -5) == Math.Pow(2, -5)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 6)} {MyPow(2, 6) == Math.Pow(2, 6)}");
-            Console.WriteLine($"{MyPow(2, -6)} {MyPow(2, -6) == Math.Pow(2, -6)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 7)} {MyPow(2, 7) == Math.Pow(2, 7)}");
-            Console.WriteLine($"{MyPow(2, -7)} {MyPow(2, -7) == Math.Pow(2, -7)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 9)} {MyPow(2, 9) == Math.Pow(2, 9)}");
-            Console.WriteLine($"{MyPow(2, -9)} {MyPow(2, -9) == Math.Pow(2, -9)}");
-            Console.WriteLine("");
-            Console.WriteLine($"{MyPow(2, 10)} {MyPow(2, 10) == Math.Pow(2, 10)}");
-            Console.WriteLine($"{MyPow(2, 10)} {MyPow(2, -10) == Math.Pow(2, -10)}");
+            Console.WriteLine($"{MyPow(2.000, -20)} {MyPow(2.000, -20) == Math.Pow(2.000, -20)}");
+            Console.WriteLine($"{MyPow(2.000, -2147483648)} {MyPow(2.000, -2147483648) == Math.Pow(2.000, -2147483648)}");
         }
 
         public static double MyPow(double x, int n)
@@ -42,44 +22,18 @@ namespace Day_22
             }
             return CalculatePower(x, n);
         }
-        public static double CalculatePower(double x, int n)
+        public static double CalculatePower(double a, int b)
         {
-            int original_n = n;
+            double y = 1;
 
-            double answer = x * x;
-            n -= 2;
-
-            if (n == 2) return answer * answer;
-            if (n == 1) return answer * x;
-            if (n == 0) return answer;
-
-            double current_min = answer;
-            int counter = n/2;
-            if (n % 2 == 1) counter++;
-            while (n > 1)
+            while (true)
             {
-                if (counter == 0)
-                {
-                    current_min = answer;
-                    counter = n / 2;
-                }
-                counter--;
-                if (n % 2 == 0)
-                {
-                    answer *= current_min;
-                    n /= 2;
-                }
-                else
-                {
-                    answer *= x;
-                    n--;
-                }
+                if ((b & 1) != 0) y = a * y;
+                b /= 2;
+                if (b == 0) return y;
+                a *= a;
             }
-            if (counter > 0) return answer * current_min;
-            if(original_n!=2 && original_n!=3 && original_n!=5 && original_n!=7)
-                if (original_n%2==0 || original_n%3==0 || original_n%5==0 || original_n%7==0)
-                    return answer * x;
-            return answer;
         }
+
     }
 }
