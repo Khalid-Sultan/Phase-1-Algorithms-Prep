faa9aba79128ca538571198363a6b319fb4bf077
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-10 14:57:23 +0300

    Added Prime Subtraction

--- Day-22/Find_Divisible.cs
+++ Day-22/Find_Divisible.cs
@@ -6,13 +6,13 @@ namespace Day_22
 {
     class Find_Divisible
     {
-        static void Main(string[] args)
+        public void FindDivisible()
         {
             int T = Convert.ToInt32(Console.ReadLine());
-            for(int i = 0; i<T; i++)
+            for (int i = 0; i < T; i++)
             {
                 string[] line = Console.ReadLine().Split(' ');
-                int l= Convert.ToInt32(line[0]);
+                int l = Convert.ToInt32(line[0]);
                 int r = Convert.ToInt32(line[1]);
                 if (l == r || l == 1) Console.WriteLine($"{l} {r}");
                 else Console.WriteLine($"{l} {2 * l}");
---
+++ Day-22/Prime_Subtraction.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_22
+{
+    class Prime_Subtraction
+    {
+        static void PrimeSubtraction()
+        {
+            int T = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                string[] line = Console.ReadLine().Split(' ');
+                Int64 l = Convert.ToInt64(line[0]);
+                Int64 r = Convert.ToInt64(line[1]);
+                Int64 difference = l - r;
+                if (difference != 1)
+                    Console.WriteLine("YES");
+                else Console.WriteLine("NO");
+            }
+        }
+    }
+}
