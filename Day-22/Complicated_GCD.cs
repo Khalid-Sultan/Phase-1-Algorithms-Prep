c65ddd026824764b5ecd25841e823298d3225132
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-10 15:55:19 +0300

    Added Complicated GCD

---
+++ Day-22/Complicated_GCD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Day_22
+{
+    class Complicated_GCD
+    {
+        static void is_complicated()
+        {
+            string[] s = Console.ReadLine().Split(' ');
+            BigInteger i = BigInteger.Parse(s[0]);
+            BigInteger j = BigInteger.Parse(s[1]);
+            if (i == j) Console.WriteLine(i);
+            else Console.WriteLine(1);
+        }
+
+    }
+}
