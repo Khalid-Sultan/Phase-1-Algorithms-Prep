06286a3f8357bab889ef059d1b2fe240a71d08c9
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-15 16:36:07 +0300

    Added Design Hashset

---
+++ Day-24/Design_Hashset.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_24
+{
+    class Design_Hashset
+    {
+        public class MyHashSet
+        {
+
+            private List<int> list;
+            /** Initialize your data structure here. */
+            public MyHashSet()
+            {
+                this.list = new List<int>();
+            }
+
+            public void Add(int key)
+            {
+                this.list.Add(key);
+            }
+
+            public void Remove(int key)
+            {
+                this.list.RemoveAll(x => x == key);
+            }
+
+            /** Returns true if this set contains the specified element */
+            public bool Contains(int key)
+            {
+                return this.list.Contains(key);
+            }
+        }
+
+        /**
+         * Your MyHashSet object will be instantiated and called as such:
+         * MyHashSet obj = new MyHashSet();
+         * obj.Add(key);
+         * obj.Remove(key);
+         * bool param_3 = obj.Contains(key);
+         */
+    }
+}
