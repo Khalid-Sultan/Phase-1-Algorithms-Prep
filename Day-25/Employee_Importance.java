8eb0e6f4d222f473c651c69c4a4bcea1f647a515
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-18 20:09:44 +0300

    Added Employee Importance

---
+++ Day-25/Employee_Importance.java
@@ -0,0 +1,29 @@
+﻿/*
+// Employee info
+class Employee {
+    // It's the unique id of each node;
+    // unique id of this employee
+    public int id;
+    // the importance value of this employee
+    public int importance;
+    // the id of direct subordinates
+    public List<Integer> subordinates;
+};
+*/
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        Map<Integer, Employee> map = new HashMap();
+        for(Employee emp : employees){
+            map.put(emp.id, emp);
+        }
+        return dfs(id, map);
+    }
+    public int dfs(int id, Map<Integer, Employee> map){
+        Employee emp = map.get(id);
+        int importance = emp.importance;
+        for(Integer subordinate : emp.subordinates){
+            importance += dfs(subordinate, map);
+        }
+        return importance;
+    }
+}
