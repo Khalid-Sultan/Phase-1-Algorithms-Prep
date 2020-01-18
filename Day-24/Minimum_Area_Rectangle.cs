d7344a80cd50943067f9746b2641663030890002
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-18 12:37:29 +0300

    Added Max Depth

---
+++ Day-24/Minimum_Area_Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_24
+{
+    class Minimum_Area_Rectangle
+    {
+        public int MinAreaRect(int[][] points)
+        {
+            Dictionary<int, List<int>> row_points = new Dictionary<int, List<int>>();
+
+            //First Collect the first row
+            foreach(int[] i in points)
+            {
+                if (row_points.ContainsKey(i[1])) row_points[i[1]].Add(i[0]);
+                else row_points.Add(i[1], new List<int>(i[0]));
+            }
+
+            //Left_Bottom
+            //Right_Bottom
+            //Left_Top
+            //Right_Top
+
+
+            return 0;
+        }
+
+    }
+}
---
+++ Day-25/Max_Depth.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_25
+{
+    class Max_Depth
+    {
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+
+            public Node(int _val)
+            {
+                val = _val;
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        } 
+        public class Solution
+        {
+            public int MaxDepth(Node root)
+            {
+                return MaxDepth(root, 1);
+            }
+
+            public int MaxDepth(Node root, int depth)
+            {
+                if (root == null) return depth - 1;
+                if (root.children.Count == 0) return depth;
+                int fin_dep = depth + 1;
+                foreach (Node child in root.children)
+                {
+                    int current_depth = MaxDepth(child, depth + 1);
+                    if (current_depth >= fin_dep)
+                    {
+                        fin_dep = current_depth;
+                    }
+                }
+                return fin_dep;
+            }
+        }
+    }
+}
