cbcfb10cd5088ea4c801dd92707b2f78299405d2
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-26 09:51:12 +0300

    Added Flood Fill

---
+++ Day-25/Flood_Fill.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_25
+{
+    class Flood_Fill
+    {
+        static int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            //First find the color in the current node
+            int color = image[sr][sc];
+
+            if (color != newColor)
+            {
+                DFS(image, sr, sc, newColor, color);
+            }
+            return image;
+        }
+
+        static void DFS(int[][] image, int sr, int sc, int newColor, int prevColor)
+        {
+            //First find the color in the current node
+            int currentColor = image[sr][sc];
+
+            if (currentColor == prevColor)
+            {
+                image[sr][sc] = newColor;
+                if (sr >= 1)
+                {
+                    DFS(image, sr - 1, sc, newColor, currentColor);
+                }
+                if (sr+1 < image.Length)
+                {
+                    DFS(image, sr + 1, sc, newColor, currentColor);
+                }
+
+                if (sc >= 1)
+                {
+                    DFS(image, sr, sc - 1, newColor, currentColor);
+                }
+                if (sc+1 < image[0].Length)
+                {
+                    DFS(image, sr, sc + 1, newColor, currentColor);
+                }
+            }
+        }
+
+        static void Main(String[] args)
+        {
+
+        }
+    }
+}
