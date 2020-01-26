1440fd01767217049b678275d2d37bcf53224c59
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-01-26 10:47:05 +0300

    Added Lowest Common Ancestor

---
+++ Day-25/Lowest_Common_Ancestor_Of_Deepest_Leaves.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Day_25.Path_Sum;
+
+namespace Day_25
+{
+    class Lowest_Common_Ancestor_Of_Deepest_Leaves
+    {
+        TreeNode ancestor = null;
+        int max_depth = 0;
+        public TreeNode LcaDeepestLeaves(TreeNode root)
+        {
+            int depth = 0;
+            if (root != null)
+                Traverse(root, depth);
+            return ancestor;
+        }
+        public int Traverse(TreeNode root, int depth)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int left_depth = Traverse(root.left, depth + 1);
+            int right_depth = Traverse(root.right, depth + 1);
+            if (left_depth == right_depth)
+            {
+                if (depth + left_depth >= max_depth)
+                {
+                    max_depth = depth + left_depth;
+                    ancestor = root;
+                }
+            }
+            if (left_depth >= right_depth)
+            {
+                return left_depth + 1;
+            }
+            return right_depth + 1;
+        }
+    }
+}
---
+++ Day-25/Sum_of_Nodes_with_Even_Valued_Grandparent.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_25
+{
+    class Sum_of_Nodes_with_Even_Valued_Grandparent
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        public int SumEvenGrandparent(TreeNode root)
+        {
+            int result = 0;
+            if (root != null)
+            {
+                result += SumEvenGrandparent(root, result);
+            }
+            return result;
+        }
+        public int SumEvenGrandparent(TreeNode root, int result)
+        {
+            if (root.val % 2 == 0)
+            {
+                if (root.left != null)
+                {
+                    if (root.left.left != null)
+                    {
+                        result += root.left.left.val;
+                    }
+                    if (root.left.right != null)
+                    {
+                        result += root.left.right.val;
+                    }
+                }
+                if (root.right != null)
+                {
+                    if (root.right.left != null)
+                    {
+                        result += root.right.left.val;
+                    }
+                    if (root.right.right != null)
+                    {
+                        result += root.right.right.val;
+                    }
+                }
+            }
+            if (root.left != null)
+            {
+                result = SumEvenGrandparent(root.left, result);
+            }
+
+            if (root.right != null)
+            {
+                result = SumEvenGrandparent(root.right, result);
+            }
+            return result;
+        }
+    }
+}
