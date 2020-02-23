fa9a33834e21dab7eb52c05ceffa098680c42002
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-02-23 12:22:11 +0300

    Added Missing Problem

---
+++ Day-39/Sorted_Squares.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_39
+{
+    class Sorted_Squares
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            List<int> list = new List<int>();
+            foreach (int i in A)
+            {
+                list.Add(i * i);
+            }
+            list.Sort();
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = list[i];
+            }
+            return A;
+        }
+    }
+}
