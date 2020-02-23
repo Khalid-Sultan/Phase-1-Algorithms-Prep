64632ec90e59b6d31fe26f0b5a231ea747e0bd24
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-02-23 12:08:18 +0300

    Missing Problem

---
+++ Day-31/2_Keys_Keyboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_31
+{
+    class _2_Keys_Keyboard
+    {
+        public int MinSteps(int n)
+        {
+            if (n == 1)
+            {
+                return 0;
+            }
+            if (n == 2)
+            {
+                return n;
+            }
+            int min = 2;
+            int temp = 0;
+            while (min < n)
+            {
+                if (n % min == 0)
+                {
+                    temp = MinSteps(n / min);
+                    break;
+                }
+                min++;
+            }
+            return temp + min;
+        }
+    }
+}
