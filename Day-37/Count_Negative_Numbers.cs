2d70acd8d0be08179da7badb3997322f4298129c
Khalid Sultan <Kalid.sultan.95@gmail.com>
2020-02-23 12:19:47 +0300

    Added Missing Problem

---
+++ Day-37/Count_Negative_Numbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_37
+{
+    class Count_Negative_Numbers
+    {
+        public int CountNegatives(int[][] grid)
+        {
+            int result = 0;
+            for (int i = grid.Length - 1; i >= 0; i--)
+            {
+                for (int j = grid[i].Length - 1; j >= 0; j--)
+                {
+                    if (grid[i][j] >= 0)
+                    {
+                        break;
+                    }
+                    result++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
